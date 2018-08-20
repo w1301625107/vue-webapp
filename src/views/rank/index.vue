0ef536871c75c55ff395040927cca3dadc023f99
wuc <wmissyou@qq.com>
2018-08-20 19:00:57 +0800

    更改榜单滚动的实现方式

--- src/views/rank/index.vue
+++ src/views/rank/index.vue
@@ -5,18 +5,18 @@
     <div class="rank_us_box">
       <h3>北美票房榜</h3>
       <p>下列是否有你的菜呢</p>
-      <div class="_scroll_wrapper">
-        <div class="_scroll">
-          <section v-for="item in us_box"
-                   class="_item"
-                   :key="item.subject.id">
-            <div class="_wrapper">
-              <img :src="item.subject.images.large"
-                   alt="">
-            </div>
-          </section>
-        </div>
+      <!-- <div class="_scroll_wrapper"> -->
+      <div class="_scroll">
+        <section v-for="item in us_box"
+                 class="_item"
+                 :key="item.subject.id">
+          <div class="_wrapper">
+            <img :src="item.subject.images.large"
+                 alt="">
+          </div>
+        </section>
       </div>
+      <!-- </div> -->
     </div>
     <div class="divide"></div>
     <div class="rank_new_movies">
@@ -35,14 +35,14 @@
       <h3 class="rank_block_header">即将上映
         <button>查看全部</button>
       </h3>
-      <div class="_scroll_wrapper">
-        <div class="_scroll">
-          <movie-list-item :item="item"
-                           clsType="type2"
-                           v-for="item in coming_soon"
-                           :key="item.id"></movie-list-item>
-        </div>
+      <!-- <div class="_scroll_wrapper"> -->
+      <div class="_scroll">
+        <movie-list-item :item="item"
+                         clsType="type2"
+                         v-for="item in coming_soon"
+                         :key="item.id"></movie-list-item>
       </div>
+      <!-- </div> -->
     </div>
   </div>
 </template>
@@ -66,7 +66,7 @@ export default {
       //console.log(resp);
     });
     this.$api.MOVIE_NEW_MOVIES("").then(resp => {
-      this.new_movies = resp.subjects.slice(0, 3);
+      this.new_movies = resp.subjects.slice(0, 9);
       //console.log(resp);
     });
     this.$api.MOVIE_US_BOX("").then(resp => {
